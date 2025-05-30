565d5c21417408d7881e753e838de0aaf0b5fdc1
nxtannnn1 <natanwill.contatar@outlook.com>
2025-05-29 21:53:36 -0300

    Commit inicial - Projeto

---
+++ src/main/java/com/sistema/restaurante/mapper/PratoMapper.java
@@ -0,0 +1,47 @@
+package com.sistema.restaurante.mapper;
+
+import com.sistema.restaurante.dto.PratoDTO;
+import com.sistema.restaurante.model.Prato;
+
+public class PratoMapper {
+
+    private PratoMapper(){
+
+    }
+
+    //Converter de Entidade p/ DTO
+
+    public static PratoDTO toDTO(Prato prato){
+        if(prato == null){
+            return null;
+        }
+
+        PratoDTO dto = new PratoDTO();
+        dto.setNome(prato.getNome());
+        dto.setCategoria(prato.getCategoria());
+        dto.setDescricao(prato.getDescricao());
+        dto.setPreco(prato.getPreco());
+        dto.setDisponibilidade(prato.getDisponibilidade());
+        dto.setUrlImagem(prato.getUrlImagem());
+
+        return dto;
+    }
+
+    //Converter de DTO p/ Entidade
+
+    public static Prato toModel (PratoDTO pratoDTO){
+        if(pratoDTO == null){
+            return null;
+        }
+
+        Prato prato = new Prato();
+        prato.setNome(pratoDTO.getNome());
+        prato.setCategoria(pratoDTO.getCategoria());
+        prato.setDescricao(pratoDTO.getDescricao());
+        prato.setPreco(pratoDTO.getPreco());
+        prato.setDisponibilidade(pratoDTO.getDisponibilidade());
+        prato.setUrlImagem(pratoDTO.getUrlImagem());
+
+        return prato;
+    }
+}
